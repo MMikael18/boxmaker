6f0770a840d4636bb33f790a486880b613b6daef
MMikael18 <mmikael_18@hotmail.com>
2016-09-17 11:16:17 +0300

    first

---
+++ app/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { Router } from 'react-router';
+//import routes from './config/routes';
+
+var Main = React.createClass({
+  render: function() {
+    return(
+      <div>app on </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+)
